916d652793571362a7c8bb697a590be2ed757eb4
heutink <heutink@marc005>
2023-06-20 14:06:34 +0200

    Initial Function

---
+++ func-parsesvc/Constants.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace func_parsesvc
+{
+    public  static  class Constants
+    {
+       public  const string FUNCTIONDESCRIPTION = @"This function will parse the Request Body for the CSV content. Based  on Input Variable (UseHeaders (bool)) will the function use the Header info from the first row. Otherwise is will use the headers supplied from the QueryString param (headers)";
+        public const string FUNCTIONSUMMARY = @"Get the JSON Array ot the CSV Content";
+
+
+        public const string OBJECTNAMEPARAMSUMMARY = @"The name of the JSON object returned";
+        public const string OBJECTNAMEPARAMDESCRIPTION = @"JSON Object name: what does one line represent e.g. { objectname : { 'key' :'value' } } ";
+
+        public const string HEADERPARAMSUMMARY = @"Comma Seperated array of HeaderNames";
+        public const string HEADERPARAMDESCRIPTION = @"Headernames of the CSV content if missing in the first line. If present those headernames will be used in the Result Objects as JSON property names ";
+
+
+        public const string SEPARATORPARAMSUMMARY = @"The field separator";
+        public const string SEPARATORPARAMDESCRIPTION = @"Give the separator is not comma for eacht field. eg: '|' or ':'  or ';' ";
+
+
+
+
+
+
+    }
+}
--- func-parsesvc/ParseCsvToJson.cs
+++ func-parsesvc/ParseCsvToJson.cs
@@ -1,23 +1,57 @@
 using System.Net;
+using System.Net.Mime;
+using System.Text.RegularExpressions;
+using func_parsesvc.Documentation;
+using FunctionLib;
+using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace func_parsesvc
 {
     public class ParseCsvToJson
     {
         private readonly ILogger _logger;
-
+        private CSVtoJSONParser _parser;
         public ParseCsvToJson(ILoggerFactory loggerFactory)
         {
             _logger = loggerFactory.CreateLogger<ParseCsvToJson>();
         }
 
+
+        [OpenApiOperation(operationId: "ParseCSVToJson", tags: new[] { "name" }, Summary = Constants.FUNCTIONSUMMARY, Description = Constants.FUNCTIONDESCRIPTION , Visibility = OpenApiVisibilityType.Important)]
+        [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "code", In = OpenApiSecurityLocationType.Header)]
+        [OpenApiParameter(name: "objectName", In = ParameterLocation.Query, Required = true, Type = typeof(string), Summary = Constants.OBJECTNAMEPARAMSUMMARY, Description = Constants.OBJECTNAMEPARAMDESCRIPTION, Visibility = OpenApiVisibilityType.Important)]
+        [OpenApiParameter(name: "csvheaders", In = ParameterLocation.Query, Required = false, Type = typeof(string), Summary = Constants.HEADERPARAMSUMMARY , Description = Constants.HEADERPARAMDESCRIPTION, Visibility = OpenApiVisibilityType.Undefined)]
+        [OpenApiParameter(name: "separator", In = ParameterLocation.Query, Required = false, Type = typeof(char), Summary = Constants.SEPARATORPARAMSUMMARY, Description = Constants.SEPARATORPARAMDESCRIPTION, Visibility = OpenApiVisibilityType.Undefined)]
+        [OpenApiRequestBody("text/csv", typeof(string))]
+
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "json", bodyType: typeof(System.Text.Json.JsonDocument), Summary = "The JSON Response" , Description = "This return the JSON Response of the CSV Payload", Example = typeof(ParametersExample))]
+        [OpenApiResponseWithoutBody(statusCode: HttpStatusCode.BadRequest, Description = "The operation was not completed successfully")]
         [Function("parsecsvtojson")]
-        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function,  "post")] HttpRequestData req)
+        public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function,  "post")] HttpRequestData req)
         {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            string query = req.Url.Query;
+            var querydict =QueryHelpers.ParseQuery(req.Url.Query);
+     
+
+            var objectName = querydict.ContainsKey("objectName") ? querydict.GetValueOrDefault("objectName").FirstOrDefault() : string.Empty;
+
+            var csvheaders = querydict.ContainsKey("csvheaders") ? querydict.GetValueOrDefault("csvheaders").FirstOrDefault() : string.Empty;
+            char separator = querydict.ContainsKey("separator") ? Convert.ToChar(querydict.GetValueOrDefault("separator").FirstOrDefault())  : ',';
+
+
+            _parser =  new CSVtoJSONParser(separator);
+            string body = await req.ReadAsStringAsync(System.Text.Encoding.Default);
+            var lines = Regex.Split(body, "\r\n|\r|\n");
+            var result =  _parser.ParseCSV(lines, objectName, csvheaders.Split(separator));
 
 
              foreach (KeyValuePair<string, IEnumerable<string>> header in req.Headers  )
@@ -28,11 +62,11 @@ namespace func_parsesvc
             }
                 
                     
-                    var response = req.CreateResponse(HttpStatusCode.OK);
-            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+             var response = req.CreateResponse(HttpStatusCode.OK );
 
-            response.WriteString("Welcome to Azure Functions!");
+            response.Headers.Add("Content-Type", "application/json; charset=utf-8");
 
+            await response.WriteStringAsync(result);
             return response;
         }
     }
